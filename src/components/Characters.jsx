608fa8bc7b7ddf4c0425bd6bedb9fdd323ddc185
Alvaro Contreras <contreras1999@gmail.com>
2023-10-30 09:34:50 +0100

    versión completa - V1

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -2,76 +2,102 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../App.css';
 import Pagination from './Pagination';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function Characters() {
-    const [nameFilter, setNameFilter] = useState('');
-    const [genderFilter, setGenderFilter] = useState('');
-    const [speciesFilter, setSpeciesFilter] = useState('');
-    const [speciesOptions, setSpeciesOptions] = useState([]);
     const [filteredCharacters, setFilteredCharacters] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const charactersPerPage = 4;
+    const [speciesOptions, setSpeciesOptions] = useState([]);
 
     useEffect(() => {
-        // Realiza una solicitud a la API para obtener todos los personajes y opciones de especie
+        // Realiza una solicitud a la API para obtener todos los personajes
         axios.get('https://rickandmortyapi.com/api/character')
             .then((response) => {
-                // Obtén la lista de especies únicas de tus personajes
-                const uniqueSpecies = [...new Set(response.data.results.map((character) => character.species))];
-                setSpeciesOptions(uniqueSpecies);
-
-                // Filtra los personajes iniciales
                 setFilteredCharacters(response.data.results);
             })
             .catch((error) => {
                 console.error('Error de conexión', error);
             });
-    }, []);
 
-    const filterCharacters = () => {
-        const newFilteredCharacters = filteredCharacters
-            .filter((character) => character.name.toLowerCase().includes(nameFilter.toLowerCase()))
-            .filter((character) => genderFilter === '' || character.gender === genderFilter)
-            .filter((character) => speciesFilter === '' || character.species === speciesFilter);
-        setFilteredCharacters(newFilteredCharacters);
-    };
+        // Realiza una solicitud para obtener las opciones de especie
+        axios.get('https://rickandmortyapi.com/api/character')
+            .then((speciesResponse) => {
+                const speciesList = speciesResponse.data.results.map((character) => character.species);
+                const uniqueSpecies = [...new Set(speciesList)];
+                setSpeciesOptions([...uniqueSpecies]); // Agrega la opción vacía y especies únicas
+            })
+            .catch((error) => {
+                console.error('Error al obtener las opciones de especie', error);
+            });
+    }, []);
 
     const handlePageClick = ({ selected }) => {
         setCurrentPage(selected + 1);
     };
 
     const offset = (currentPage - 1) * charactersPerPage;
-    const currentCharacters = filteredCharacters.slice(offset, offset + charactersPerPage);
 
     return (
         <>
             <h1>Personajes de Rick&Morty</h1>
-            <div className="filtro">
-                <h2>Formulario de filtros</h2>
-                <input
-                    type="text"
-                    placeholder="Filtrar por nombre"
-                    value={nameFilter}
-                    onChange={(e) => setNameFilter(e.target.value)}
-                />
-                <select value={genderFilter} onChange={(e) => setGenderFilter(e.target.value)}>
-                    <option value="">Filtrar por género</option>
-                    <option value="Male">Masculino</option>
-                    <option value="Female">Femenino</option>
-                    <option value="unknown">Desconocido</option>
-                </select>
-                <select value={speciesFilter} onChange={(e) => setSpeciesFilter(e.target.value)}>
-                    <option value="">Filtrar por especie</option>
-                    {speciesOptions.map((species) => (
-                        <option key={species} value={species}>
-                            {species}
-                        </option>
-                    ))}
-                </select>
-                <button onClick={filterCharacters}>Buscar</button>
-            </div>
+            <Formik
+                initialValues={{ name: '', gender: '', species: '' }}
+                onSubmit={(values) => {
+                    let newFilteredCharacters = filteredCharacters;
+
+                    // Realiza los filtros basados en los valores del formulario
+                    if (values.name) {
+                        newFilteredCharacters = newFilteredCharacters.filter((character) =>
+                            character.name.toLowerCase().includes(values.name.toLowerCase())
+                        );
+                    }
+
+                    if (values.gender) {
+                        newFilteredCharacters = newFilteredCharacters.filter((character) =>
+                            character.gender === values.gender
+                        );
+                    }
+
+                    if (values.species) {
+                        newFilteredCharacters = newFilteredCharacters.filter((character) =>
+                            character.species === values.species
+                        );
+                    }
+
+                    // Actualiza el estado con los personajes filtrados
+                    setFilteredCharacters(newFilteredCharacters);
+
+                    // Reinicia la paginación
+                    setCurrentPage(1);
+                }}
+            >
+                {() => (
+                    <Form>
+                        <div className="filtro">
+                            <h2>Formulario de filtros</h2>
+                            <Field type="text" name="name" placeholder="Filtrar por nombre" />
+                            <Field as="select" name="gender">
+                                <option value="">Filtrar por género</option>
+                                <option value="Male">Masculino</option>
+                                <option value="Female">Femenino</option>
+                                <option value="unknown">Desconocido</option>
+                            </Field>
+                            <Field as="select" name="species">
+                                <option value="">Filtrar por especie</option>
+                                {speciesOptions.map((species) => (
+                                    <option key={species} value={species}>
+                                        {species}
+                                    </option>
+                                ))}
+                            </Field>
+                            <button type="submit">Buscar</button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
             <div className='padre'>
-                {currentCharacters.map((character) => (
+                {filteredCharacters.slice(offset, offset + charactersPerPage).map((character) => (
                     <div key={character.id} className='contenedor'>
                         <h2>{character.id}. {character.name}</h2>
                         <div className='carta'>

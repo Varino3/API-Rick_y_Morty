66783f65811732505498a5f11e4d2917b14631a1
Alvaro Contreras <contreras1999@gmail.com>
2023-10-30 12:22:29 +0100

    version V1 - tema oscuro/claro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 // Mostrar APP por pantalla
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   return (
-    <div className="App">
-      <Card />
+    <div>
+      <button onClick={toggleTheme}><FontAwesomeIcon icon={faSun} /> / <FontAwesomeIcon icon={faMoon} /></button>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href={
+          isDarkTheme
+            ? 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css'
+            : 'https://cdn.jsdelivr.net/npm/water.css@2/out/light.css'
+        }
+      />
+      <div className="App">
+        <Card />
+      </div>
     </div>
   );
 }
